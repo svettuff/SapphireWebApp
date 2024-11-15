a98ecda4f73d9fd9ddcf7c119936af4ef54f6f05
svettuff <arkadii.nagogaw@gmail.com>
2024-11-15 18:16:28 +0100

    logic

--- index.js
+++ index.js
@@ -17,31 +17,31 @@ async function selectTopic(topic) {
         const data = await response.json();
         const answer = data.answer || "Error with getting data";
 
-        // Перенаправляем на новую страницу с ответом
-        window.location.href = `playground.html?response=${encodeURIComponent(answer)}`;
+        // Сохранение ответа в Local Storage
+        localStorage.setItem('response', answer);
+
+        // Перенаправление на новую страницу
+        window.location.href = 'playground.html';
     } catch (error) {
         document.getElementById('response').innerText = `Error: ${error.message}`;
         console.error("Error:", error);
     }
 }
 
-function getQueryParam(param) {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get(param);
-}
-
 document.addEventListener('DOMContentLoaded', () => {
-    const responseText = new URLSearchParams(window.location.search).get('response');
     const responseElement = document.getElementById('response-text');
+
+    // Получаем ответ из Local Storage
+    const responseText = localStorage.getItem('response');
 
     if (responseText) {
         // Преобразуем текст с Markdown в HTML
-        responseElement.innerHTML = marked.parse(decodeURIComponent(responseText));
+        responseElement.innerHTML = marked.parse(responseText);
 
         // Подсвечиваем синтаксис C++ в коде
         const codeBlocks = responseElement.querySelectorAll('pre code');
         codeBlocks.forEach((block) => {
-            hljs.highlightElement(block); // Используем highlight.js для подсветки
+            hljs.highlightElement(block);
         });
     } else {
         responseElement.innerText = "Ответ не найден.";
