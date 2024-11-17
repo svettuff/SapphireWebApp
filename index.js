104b4d96ebc83c312f9b55335049cf255f19f3b4
svettuff <arkadii.nagogaw@gmail.com>
2024-11-17 14:55:33 +0100

    payment

--- index.js
+++ index.js
@@ -1,63 +1,3 @@
-// Old server stuff, will be reusable in future with playground
-
-//async function selectTopic(topic) {
-//    const responseElement = document.getElementById('response');
-//
-//    // Проверяем, существует ли элемент с id 'response'
-//    if (responseElement) {
-//        responseElement.innerText = "Loading...";
-//    }
-//
-//    try {
-//        const response = await fetch('https://sapphireserver.almandine.ch:5000/get_response', {
-//            method: 'POST',
-//            headers: {
-//                'Content-Type': 'application/json'
-//            },
-//            body: JSON.stringify({ topic: topic })
-//        });
-//
-//        if (!response.ok) {
-//            throw new Error(`Error HTTP: ${response.status}`);
-//        }
-//
-//        const data = await response.json();
-//        const answer = data.answer || "Error with getting data";
-//
-//        // Сохранение ответа в Local Storage
-//        localStorage.setItem('response', answer);
-//
-//        // Перенаправление на новую страницу
-//        window.location.href = 'playground.html';
-//    } catch (error) {
-//        // Проверяем, существует ли элемент с id 'response'
-//        if (responseElement) {
-//            responseElement.innerText = `Error: ${error.message}`;
-//        }
-//        console.error("Error:", error);
-//    }
-//}
-//
-//document.addEventListener('DOMContentLoaded', () => {
-//    const responseElement = document.getElementById('response-text');
-//
-//    // Получаем ответ из Local Storage
-//    const responseText = localStorage.getItem('response');
-//
-//    if (responseText) {
-//        // Преобразуем текст с Markdown в HTML
-//        responseElement.innerHTML = marked.parse(responseText);
-//
-//        // Подсвечиваем синтаксис C++ в коде
-//        const codeBlocks = responseElement.querySelectorAll('pre code');
-//        codeBlocks.forEach((block) => {
-//            hljs.highlightElement(block);
-//        });
-//    } else {
-//        responseElement.innerText = "No Response.";
-//    }
-//});
-
 async function selectTopic(fileName) {
     try {
         const fileUrl = `Themes/${fileName}`;
@@ -79,145 +19,148 @@ async function selectTopic(fileName) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const responseElement = document.getElementById('response-text');
-
-    const responseText = localStorage.getItem('theme');
-
-    if (responseText) {
-        responseElement.innerHTML = marked.parse(responseText);
-
-        const codeBlocks = responseElement.querySelectorAll('pre code');
-        codeBlocks.forEach((block) => {
-            hljs.highlightElement(block);
-        });
-    } else {
-        responseElement.innerText = "No theme.";
+    if (window.location.pathname === '/Sapphire/index.html') {
+        const urlParams = new URLSearchParams(window.location.search);
+        const userId = urlParams.get('user_id');
+        alert('User ID: ' + userId);
     }
 
-    const editor = CodeMirror.fromTextArea(document.getElementById('code-editor'), {
-        mode: 'text/x-c++src',
-        matchBrackets: true,
-        theme: 'playground',
-    });
+    if (window.location.pathname === '/Sapphire/playground.html') {
+        const responseElement = document.getElementById('response-text');
 
-    const inputEditor = CodeMirror.fromTextArea(document.getElementById('input'), { theme: 'playground', lineWrapping: true });
-    const outputEditor = CodeMirror.fromTextArea(document.getElementById('output'), { readOnly: 'nocursor', theme: 'playground', lineWrapping: true });
-    const questionEditor = CodeMirror.fromTextArea(document.getElementById('question'), { theme: 'playground', lineWrapping: true });
+        const responseText = localStorage.getItem('theme');
 
-    // Применение ваших стилей к CodeMirror
-    const editorWrapper = editor.getWrapperElement();
-    editorWrapper.style.fontFamily = '"Courier New", Courier, monospace';
-    editorWrapper.style.fontSize = '17px';
-    editorWrapper.style.padding = '2px';
+        if (responseText) {
+            responseElement.innerHTML = marked.parse(responseText);
 
-    const inputWrapper = inputEditor.getWrapperElement();
-    inputWrapper.style.fontFamily = '"Courier New", Courier, monospace';
-    inputWrapper.style.fontSize = '17px';
-    inputWrapper.style.padding = '2px';
-    inputWrapper.style.height = '50px';
-
-    const outputWrapper = outputEditor.getWrapperElement();
-    outputWrapper.style.fontFamily = '"Courier New", Courier, monospace';
-    outputWrapper.style.fontSize = '17px';
-    outputWrapper.style.padding = '2px';
-    outputWrapper.style.height = '150px';
-
-    const questionWrapper = questionEditor.getWrapperElement();
-    questionWrapper.style.fontFamily = '"Courier New", Courier, monospace';
-    questionWrapper.style.fontSize = '17px';
-    questionWrapper.style.padding = '2px';
-    questionWrapper.style.height = '300px';
-
-    const runButton = document.getElementById('run-button');
-    runButton.addEventListener('click', async () => {
-        const code = editor.getValue();
-        let input = inputEditor.getValue();
-        input = input.split(',').map(item => item.trim()).join('\n');
-
-        try {
-            const response = await fetch('https://sapphireserver.almandine.ch:5000/execute_cpp', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ code: code, input: input })
+            const codeBlocks = responseElement.querySelectorAll('pre code');
+            codeBlocks.forEach((block) => {
+                hljs.highlightElement(block);
             });
+        } else {
+            responseElement.innerText = "No theme.";
+        }
 
-            if (!response.ok) {
-                throw new Error(`HTTP Error: ${response.status}`);
+        const editor = CodeMirror.fromTextArea(document.getElementById('code-editor'), {
+            mode: 'text/x-c++src',
+            matchBrackets: true,
+            theme: 'playground',
+        });
+
+        const inputEditor = CodeMirror.fromTextArea(document.getElementById('input'), { theme: 'playground', lineWrapping: true });
+        const outputEditor = CodeMirror.fromTextArea(document.getElementById('output'), { readOnly: 'nocursor', theme: 'playground', lineWrapping: true });
+        const questionEditor = CodeMirror.fromTextArea(document.getElementById('question'), { theme: 'playground', lineWrapping: true });
+
+        // Применение ваших стилей к CodeMirror
+        const editorWrapper = editor.getWrapperElement();
+        editorWrapper.style.fontFamily = '"Courier New", Courier, monospace';
+        editorWrapper.style.fontSize = '17px';
+        editorWrapper.style.padding = '2px';
+
+        const inputWrapper = inputEditor.getWrapperElement();
+        inputWrapper.style.fontFamily = '"Courier New", Courier, monospace';
+        inputWrapper.style.fontSize = '17px';
+        inputWrapper.style.padding = '2px';
+        inputWrapper.style.height = '50px';
+
+        const outputWrapper = outputEditor.getWrapperElement();
+        outputWrapper.style.fontFamily = '"Courier New", Courier, monospace';
+        outputWrapper.style.fontSize = '17px';
+        outputWrapper.style.padding = '2px';
+        outputWrapper.style.height = '150px';
+
+        const questionWrapper = questionEditor.getWrapperElement();
+        questionWrapper.style.fontFamily = '"Courier New", Courier, monospace';
+        questionWrapper.style.fontSize = '17px';
+        questionWrapper.style.padding = '2px';
+        questionWrapper.style.height = '300px';
+
+        const runButton = document.getElementById('run-button');
+        runButton.addEventListener('click', async () => {
+            const code = editor.getValue();
+            let input = inputEditor.getValue();
+            input = input.split(',').map(item => item.trim()).join('\n');
+
+            try {
+                const response = await fetch('https://sapphireserver.almandine.ch:5000/execute_cpp', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ code: code, input: input })
+                });
+
+                if (!response.ok) {
+                    throw new Error(`HTTP Error: ${response.status}`);
+                }
+
+                const data = await response.json();
+                const output = data.output || "Error with executing code";
+
+                outputEditor.setValue(output);
+            } catch (error) {
+                outputEditor.setValue(`Error: ${error.message}`);
+                console.error("Error:", error);
             }
+        });
 
-            const data = await response.json();
-            const output = data.output || "Error with executing code";
+        const askButton = document.getElementById('ask-button');
+        askButton.addEventListener('click', async () => {
+            const question = questionEditor.getValue();
 
-            outputEditor.setValue(output);
-        } catch (error) {
-            outputEditor.setValue(`Error: ${error.message}`);
-            console.error("Error:", error);
-        }
-    });
+            try {
+                const response = await fetch('https://sapphireserver.almandine.ch:5000/ask', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ question: question })
+                });
 
-    const askButton = document.getElementById('ask-button');
-    askButton.addEventListener('click', async () => {
-        const question = questionEditor.getValue();
+                if (!response.ok) {
+                    throw new Error(`HTTP Error: ${response.status}`);
+                }
 
-        try {
-              const response = await fetch('https://sapphireserver.almandine.ch:5000/ask', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ question: question })
-            });
+                const data = await response.json();
+                const answer = data.answer || "Error with getting data";
 
-            if (!response.ok) {
-                throw new Error(`HTTP Error: ${response.status}`);
+                questionEditor.setValue(answer);
             }
-
-            const data = await response.json();
-            const answer = data.answer || "Error with getting data";
-
-            questionEditor.setValue(answer);
-        }
-        catch (error) {
-            console.error("Error:", error);
-        }
-    })
+            catch (error) {
+                console.error("Error:", error);
+            }
+        })
+    }
 });
 
 
-let lastScrollY = 0; // Переменная для отслеживания предыдущей позиции прокрутки
+let lastScrollY = 0;
 
 window.addEventListener("scroll", function () {
     const currentScrollY = window.scrollY || window.pageYOffset;
 
-    // Проверяем, если разница в прокрутке больше 50 пикселей
     if (Math.abs(currentScrollY - lastScrollY) > 50) {
-        document.activeElement.blur(); // Закрываем клавиатуру
+        document.activeElement.blur();
     }
 
-    // Обновляем значение последней позиции прокрутки
     lastScrollY = currentScrollY;
 });
 
 const extraSpace = document.createElement('div');
 extraSpace.style.height = '300px';
-extraSpace.style.visibility = 'hidden'; // Делаем его невидимым
+extraSpace.style.visibility = 'hidden';
 document.body.appendChild(extraSpace);
 
 document.addEventListener('focusin', (event) => {
     const element = event.target.closest('.input-output, .CodeMirror');
 
     if (element) {
-        // Вычисляем положение элемента и высоту окна
         const viewportHeight = window.innerHeight;
         const elementRect = element.getBoundingClientRect();
         const elementBottom = elementRect.bottom + window.scrollY;
 
-        // Вычисляем позицию, чтобы нижняя часть элемента была в центре экрана
         const scrollPosition = elementBottom - viewportHeight / 2;
 
-        // Прокручиваем страницу к нужной позиции
         window.scrollTo({
             top: scrollPosition,
             behavior: 'smooth'
@@ -226,5 +169,5 @@ document.addEventListener('focusin', (event) => {
 });
 
 document.addEventListener('focusout', () => {
-    // При потере фокуса ничего не делаем, чтобы не вызывать ненужных артефактов
+
 });
