b20665976198522782f19afaf636bef16a287511
svettuff <arkadii.nagogaw@gmail.com>
2024-11-18 13:41:55 +0100

    refactor

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
-async function selectTopic(fileName) {
-    const topicButton = event.target;
-    if (topicButton.classList.contains('locked')) {
-        document.getElementById('modal-overlay').style.display = 'flex';
-        return;
-    }
+////////////////////////////////////////////* Functions *////////////////////////////////////////////
 
-    try {
-        const fileUrl = `Themes/${fileName}`;
-
-        const response = await fetch(fileUrl);
-
-        if (!response.ok) {
-            throw new Error(`Error fetching file: ${response.status}`);
-        }
-
-        const markdownText = await response.text();
-
-        localStorage.setItem('theme', markdownText);
-
-        window.location.href = 'playground.html';
-    } catch (error) {
-        console.error("Error:", error);
-    }
-}
-
-function unlockAllTopics() {
+function unlockAllTopics()
+{
     const lockedTopics = document.querySelectorAll('.topic-button.locked');
     lockedTopics.forEach(topic => {
         topic.classList.remove('locked');
     });
+}
+
+////////////////////////////////////////////* Async Functions *////////////////////////////////////////////
+
+async function selectTopic(fileName)
+{
+    const topicButton = event.target;
+    if (topicButton.classList.contains('locked'))
+    {
+        document.getElementById('modal-overlay').style.display = 'flex';
+        return;
+    }
+    try
+    {
+        const fileUrl = `Themes/${fileName}`;
+        const response = await fetch(fileUrl);
+
+        if (!response.ok)
+        {
+            throw new Error(`Error fetching file: ${response.status}`);
+        }
+
+        const markdownText = await response.text();
+        localStorage.setItem('theme', markdownText);
+        window.location.href = 'playground.html';
+    }
+    catch (error)
+    {
+        console.error("Error:", error);
+    }
 }
 
 async function checkUserPayment()
@@ -36,8 +43,10 @@ async function checkUserPayment()
     const tg = window.Telegram.WebApp;
 
     const user = tg.initDataUnsafe?.user;
-    if (user) {
-        try {
+    if (user)
+    {
+        try
+        {
             const response = await fetch('https://sapphireserver.almandine.ch:443/check-user-payment', {
                 method: 'POST',
                 headers: {
@@ -46,40 +55,133 @@ async function checkUserPayment()
                 body: JSON.stringify({id: user})
             });
 
-            if (!response.ok) {
+            if (!response.ok)
+            {
                 throw new Error(`HTTP Error: ${response.status}`);
             }
 
             const data = await response.json();
             const userExists = data.exists;
 
-            if (userExists) {
+            if (userExists)
+            {
                 unlockAllTopics();
             }
-        } catch (error) {
+        }
+        catch (error)
+        {
             console.error("Error:", error);
         }
     }
 }
 
+async function createPaymentLink()
+{
+    try
+    {
+        const response = await fetch('https://sapphireserver.almandine.ch:443/generate-invoice', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({})
+        });
+
+        const data = await response.json();
+        if (data.invoiceLink)
+        {
+            if (window.Telegram && window.Telegram.WebApp)
+            {
+                window.Telegram.WebApp.openInvoice(data.invoiceLink);
+                window.Telegram.WebApp.onEvent('invoiceClosed', async (event) => {
+                        document.getElementById('modal-overlay').style.display = 'none';
+                        await checkUserPayment();
+                });
+            }
+            else
+            {
+                window.open(data.invoiceLink, "_blank");
+            }
+        }
+        else
+        {
+            console.error("Error creating payment link:", data.error);
+        }
+    }
+    catch (error)
+    {
+        console.error("Error performing request:", error);
+    }
+}
+
+////////////////////////////////////////////* Events *////////////////////////////////////////////
+
+let lastScrollY = 0;
+
+window.addEventListener("scroll", function () {
+    const currentScrollY = window.scrollY || window.pageYOffset;
+
+    if (Math.abs(currentScrollY - lastScrollY) > 50)
+    {
+        document.activeElement.blur();
+    }
+
+    lastScrollY = currentScrollY;
+});
+
+const extraSpace = document.createElement('div');
+extraSpace.style.height = '300px';
+extraSpace.style.visibility = 'hidden';
+document.body.appendChild(extraSpace);
+
+document.addEventListener('focusin', (event) => {
+    const element = event.target.closest('.input-output, .CodeMirror');
+
+    if (element)
+    {
+        const viewportHeight = window.innerHeight;
+        const elementRect = element.getBoundingClientRect();
+        const elementBottom = elementRect.bottom + window.scrollY;
+        const scrollPosition = elementBottom - viewportHeight / 2;
+
+        window.scrollTo(
+            {
+            top: scrollPosition,
+            behavior: 'smooth'
+        });
+    }
+});
+
+document.getElementById("wallet-button").addEventListener("click", createPaymentLink);
+
+document.addEventListener('focusout', () => {});
+
+document.getElementById('modal-overlay').addEventListener('click', function(event) {
+    if (event.target === this)
+    {
+        document.getElementById('modal-overlay').style.display = 'none';
+    }
+});
+
 document.addEventListener('DOMContentLoaded', async () => {
-    if (!window.location.pathname.includes('playground.html')) {
+    if (!window.location.pathname.includes('playground.html'))
+    {
         await checkUserPayment();
     }
 
-    if (window.location.pathname.includes('playground.html')) {
+    if (window.location.pathname.includes('playground.html'))
+    {
         const responseElement = document.getElementById('response-text');
-
         const responseText = localStorage.getItem('theme');
 
-        if (responseText) {
+        if (responseText)
+        {
             responseElement.innerHTML = marked.parse(responseText);
-
             const codeBlocks = responseElement.querySelectorAll('pre code');
             codeBlocks.forEach((block) => {
                 hljs.highlightElement(block);
             });
-        } else {
+        }
+        else
+        {
             responseElement.innerText = "No theme.";
         }
 
@@ -93,7 +195,6 @@ document.addEventListener('DOMContentLoaded', async () => {
         const outputEditor = CodeMirror.fromTextArea(document.getElementById('output'), { readOnly: 'nocursor', theme: 'playground', lineWrapping: true });
         const questionEditor = CodeMirror.fromTextArea(document.getElementById('question'), { theme: 'playground', lineWrapping: true });
 
-        // Применение ваших стилей к CodeMirror
         const editorWrapper = editor.getWrapperElement();
         editorWrapper.style.fontFamily = '"Courier New", Courier, monospace';
         editorWrapper.style.fontSize = '17px';
@@ -123,7 +224,8 @@ document.addEventListener('DOMContentLoaded', async () => {
             let input = inputEditor.getValue();
             input = input.split(',').map(item => item.trim()).join('\n');
 
-            try {
+            try
+            {
                 const response = await fetch('https://sapphireserver.almandine.ch:443/execute_cpp', {
                     method: 'POST',
                     headers: {
@@ -132,15 +234,17 @@ document.addEventListener('DOMContentLoaded', async () => {
                     body: JSON.stringify({ code: code, input: input })
                 });
 
-                if (!response.ok) {
+                if (!response.ok)
+                {
                     throw new Error(`HTTP Error: ${response.status}`);
                 }
 
                 const data = await response.json();
                 const output = data.output || "Error with executing code";
-
                 outputEditor.setValue(output);
-            } catch (error) {
+            }
+            catch (error)
+            {
                 outputEditor.setValue(`Error: ${error.message}`);
                 console.error("Error:", error);
             }
@@ -150,7 +254,8 @@ document.addEventListener('DOMContentLoaded', async () => {
         askButton.addEventListener('click', async () => {
             const question = questionEditor.getValue();
 
-            try {
+            try
+            {
                 const response = await fetch('https://sapphireserver.almandine.ch:443/ask', {
                     method: 'POST',
                     headers: {
@@ -159,97 +264,19 @@ document.addEventListener('DOMContentLoaded', async () => {
                     body: JSON.stringify({ question: question })
                 });
 
-                if (!response.ok) {
+                if (!response.ok)
+                {
                     throw new Error(`HTTP Error: ${response.status}`);
                 }
 
                 const data = await response.json();
                 const answer = data.answer || "Error with getting data";
-
                 questionEditor.setValue(answer);
             }
-            catch (error) {
+            catch (error)
+            {
                 console.error("Error:", error);
             }
         })
     }
 });
-
-let lastScrollY = 0;
-
-window.addEventListener("scroll", function () {
-    const currentScrollY = window.scrollY || window.pageYOffset;
-
-    if (Math.abs(currentScrollY - lastScrollY) > 50) {
-        document.activeElement.blur();
-    }
-
-    lastScrollY = currentScrollY;
-});
-
-const extraSpace = document.createElement('div');
-extraSpace.style.height = '300px';
-extraSpace.style.visibility = 'hidden';
-document.body.appendChild(extraSpace);
-
-document.addEventListener('focusin', (event) => {
-    const element = event.target.closest('.input-output, .CodeMirror');
-
-    if (element) {
-        const viewportHeight = window.innerHeight;
-        const elementRect = element.getBoundingClientRect();
-        const elementBottom = elementRect.bottom + window.scrollY;
-
-        const scrollPosition = elementBottom - viewportHeight / 2;
-
-        window.scrollTo({
-            top: scrollPosition,
-            behavior: 'smooth'
-        });
-    }
-});
-
-document.addEventListener('focusout', () => {
-
-});
-
-document.getElementById('modal-overlay').addEventListener('click', function(event) {
-    if (event.target === this) {
-        document.getElementById('modal-overlay').style.display = 'none';
-    }
-});
-
-async function createPaymentLink() {
-    try {
-        const response = await fetch('https://sapphireserver.almandine.ch:443/generate-invoice', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({})
-        });
-
-        const data = await response.json();
-        if (data.invoiceLink) {
-            // Используем Telegram WebApp API для открытия ссылки внутри Telegram
-            if (window.Telegram && window.Telegram.WebApp) {
-                window.Telegram.WebApp.openInvoice(data.invoiceLink);
-
-                // Подписываемся на событие закрытия инвойса
-                window.Telegram.WebApp.onEvent('invoiceClosed', async (event) => {
-                        document.getElementById('modal-overlay').style.display = 'none';
-                        await checkUserPayment();
-                });
-            } else {
-                // Открываем в новом окне, если WebApp API недоступен
-                window.open(data.invoiceLink, "_blank");
-            }
-        } else {
-            console.error("Ошибка при создании платёжной ссылки:", data.error);
-        }
-    } catch (error) {
-        console.error("Ошибка при выполнении запроса:", error);
-    }
-}
-
-
-// Пример вызова функции при нажатии на кнопку
-document.getElementById("wallet-button").addEventListener("click", createPaymentLink);
